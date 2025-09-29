9296607910eec16bbef6de4fab0094655af74c03
Maikon Sposito <mknsposito@gmail.com>
2025-09-29 12:47:04 -0300

    feat: criação do projeto e configuração docker-compose para Postgres

---
+++ microsoft-calendar-insights-ai/src/main/java/br/com/maikonspo/microsoftAI/MicrosoftCalendarInsightsAiApplication.java
@@ -0,0 +1,13 @@
+package br.com.maikonspo.microsoftAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrosoftCalendarInsightsAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrosoftCalendarInsightsAiApplication.class, args);
+	}
+
+}
---
+++ microsoft-calendar-insights-ai/src/test/java/br/com/maikonspo/microsoftAI/MicrosoftCalendarInsightsAiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.maikonspo.microsoftAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrosoftCalendarInsightsAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
